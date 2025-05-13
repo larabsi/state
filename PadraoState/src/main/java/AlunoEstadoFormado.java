edae97654d71628466e17fc7ab9d4ccda22fbec5
Lara Ferreira <117956372+larabsi@users.noreply.github.com>
2025-05-13 10:11:38 -0300

    Add files via upload

---
+++ PadraoState/src/main/java/AlunoEstadoFormado.java
@@ -0,0 +1,17 @@
+public class AlunoEstadoFormado  extends AlunoEstado {
+
+    private AlunoEstadoFormado() {
+    }
+
+    ;
+    private static AlunoEstadoFormado instance = new AlunoEstadoFormado();
+
+    public static AlunoEstadoFormado getInstance() {
+        return instance;
+    }
+
+    public String getEstado() {
+        return "Formado";
+    }
+
+}
